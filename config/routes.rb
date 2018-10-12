ad79785b818c7da7a527c07d5ae1e8c1b8b3a8db
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-10-12 16:36:10 -0500

    API de articulos en Rails

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :articulos
+  post 'user_token' => 'user_token#create'
+  resources :users
+  # get /users/ to: users#index
+  # get /users/:id to users#show
+  # post /users/ to users#create
+  # put /users/:id to users#update
+  # delete /users/:id to users#destroy
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
